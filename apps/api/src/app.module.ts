168bca0aede2e702b152700d6a4b0135f2fba688
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-09 11:49:21 +0100

    refactor(api): use global database module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
-import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { EmployeesModule } from './employees/employees.module';
 import { OrganizationModule } from './organization/organization.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, EmployeesModule, OrganizationModule],
+  imports: [DatabaseModule, AuthModule, EmployeesModule, OrganizationModule],
   controllers: [HealthController],
-  providers: [PrismaService],
-  exports: [PrismaService],
 })
 export class AppModule {}
