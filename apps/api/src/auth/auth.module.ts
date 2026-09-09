2313768f866211e804f3f0ba96f4742fb7eb2da8
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-09 12:22:16 +0100

    refactor: use global database module for auth

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '../prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SessionGuard } from './guards/session.guard';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, SessionGuard, PrismaService],
+  providers: [AuthService, SessionGuard],
   exports: [AuthService, SessionGuard],
 })
 export class AuthModule {}
