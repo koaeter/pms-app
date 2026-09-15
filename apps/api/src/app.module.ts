f5bd2f2513420853550202119bd6e42ab2c7325b
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 15:53:02 +0100

    feat: register administration module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,11 +5,12 @@ import { HealthController } from './health.controller';
 import { AuthModule } from './auth/auth.module';
 import { OrganisationModule } from './organisation/organisation.module';
 import { PerformanceModule } from './performance/performance.module';
+import { AdminModule } from './admin/admin.module';
 import { SessionGuard } from './auth/session.guard';
 import { PermissionGuard } from './auth/permission.guard';
 
 @Module({
-  imports: [AuthModule, OrganisationModule, PerformanceModule],
+  imports: [AuthModule, OrganisationModule, PerformanceModule, AdminModule],
   controllers: [HealthController],
   providers: [PrismaService, { provide: APP_GUARD, useClass: SessionGuard }, { provide: APP_GUARD, useClass: PermissionGuard }],
   exports: [PrismaService],
