a5638639b1ed747c6772b2a161cf3b1348cb050b
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 15:09:22 +0100

    feat: register performance module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from './prisma.service';
 import { HealthController } from './health.controller';
 import { AuthModule } from './auth/auth.module';
 import { OrganisationModule } from './organisation/organisation.module';
+import { PerformanceModule } from './performance/performance.module';
 
 @Module({
-  imports: [AuthModule, OrganisationModule],
+  imports: [AuthModule, OrganisationModule, PerformanceModule],
   controllers: [HealthController],
   providers: [PrismaService],
   exports: [PrismaService],
