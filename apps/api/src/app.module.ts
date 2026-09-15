f919084b202bb3bf2a3f16cf18ce3d3d65c74531
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 14:57:15 +0100

    feat: register organisation module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { HealthController } from './health.controller';
+import { OrganisationModule } from './organisation/organisation.module';
 
 @Module({
+  imports: [OrganisationModule],
   controllers: [HealthController],
   providers: [PrismaService],
   exports: [PrismaService],
