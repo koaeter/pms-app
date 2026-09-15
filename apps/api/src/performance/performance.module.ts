f3f913e15eb544ac3190c22a81e78c99251b1603
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 15:45:57 +0100

    feat: register performance access service

--- apps/api/src/performance/performance.module.ts
+++ apps/api/src/performance/performance.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { PerformanceController } from './performance.controller';
 import { PerformanceService } from './performance.service';
+import { PerformanceAccessService } from './performance-access.service';
 
 @Module({
   controllers: [PerformanceController],
-  providers: [PerformanceService, PrismaService],
+  providers: [PerformanceService, PerformanceAccessService, PrismaService],
 })
 export class PerformanceModule {}
