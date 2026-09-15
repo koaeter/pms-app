877c9f4a1a85593f54366cd14b5b8dab70045441
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 14:56:38 +0100

    feat: add organisation module

---
+++ apps/api/src/organisation/organisation.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { OrganisationController } from './organisation.controller';
+import { OrganisationService } from './organisation.service';
+
+@Module({ controllers: [OrganisationController], providers: [OrganisationService] })
+export class OrganisationModule {}
