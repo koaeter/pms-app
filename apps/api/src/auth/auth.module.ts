8a34aaad291a7df7f2ec8262d606abb48c36a4e9
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 15:44:29 +0100

    feat: register permission guard

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SessionGuard } from './session.guard';
+import { PermissionGuard } from './permission.guard';
 
-@Module({ controllers: [AuthController], providers: [AuthService, SessionGuard], exports: [AuthService, SessionGuard] })
+@Module({ controllers: [AuthController], providers: [AuthService, SessionGuard, PermissionGuard], exports: [AuthService, SessionGuard, PermissionGuard] })
 export class AuthModule {}
