b6cc11df8f90bd8e2d4e4e5100194a9f9137f229
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-15 14:58:55 +0100

    feat: register authentication and organisation modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { HealthController } from './health.controller';
+import { AuthModule } from './auth/auth.module';
 import { OrganisationModule } from './organisation/organisation.module';
 
 @Module({
-  imports: [OrganisationModule],
+  imports: [AuthModule, OrganisationModule],
   controllers: [HealthController],
   providers: [PrismaService],
   exports: [PrismaService],
