de13540bed95942b1f18a713e025f91ff7f6b9e7
koaeter <116206164+koaeter@users.noreply.github.com>
2026-09-23 10:39:49 +0100

    fix(identity): provide audit service to organisation provisioning

--- apps/api/src/organisation/organisation.module.ts
+++ apps/api/src/organisation/organisation.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { OrganisationController } from './organisation.controller';
 import { OrganisationService } from './organisation.service';
+import { AuditService } from '../audit.service';
 
-@Module({ controllers: [OrganisationController], providers: [OrganisationService] })
+@Module({ controllers: [OrganisationController], providers: [OrganisationService, AuditService] })
 export class OrganisationModule {}
